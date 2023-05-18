139232eb3e4b96ca4b43022cf24b00dbace21712
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-18 10:21:14 +0530

    [Mazhar]ADDED: Student Controllers To The Project

---
+++ project/src/controllers/student.controller.js
@@ -0,0 +1,108 @@
+import HttpStatus from 'http-status-codes';
+import * as UserService from '../services/user.service';
+import * as StudentService from '../services/student.service';
+
+
+export const availableCourses = async (req, res, next) => {
+  try {
+    const data = await StudentService.availableCourses(req.params.email);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'All courses fetched successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getImageById = async (req, res, next) => {
+  try {
+    const data = await StudentService.getImageById(req.params.id);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'All courses fetched successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const myCourses = async (req, res, next) => {
+    try {
+      const data = await StudentService.myCourses(req.params.email);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'All courses fetched successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const enrollInCourse = async (req, res, next) => {
+    try {
+      const data = await StudentService.enrollInCourse(req.body);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Course Enrollment Successful'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const getQuiz = async (req, res, next) => {
+    try {
+      const data = await StudentService.getQuiz(req.params.courseId);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Course Enrollment Successful'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const submitQuiz = async (req, res, next) => {
+    try {
+      const data = await StudentService.submitQuiz(req);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Course Enrollment Successful'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const getHighestMarks = async (req, res, next) => {
+    try {
+      const data = await StudentService.getHighestMarks(req);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Course Enrollment Successful'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const cancelCourse = async (req, res, next) => {
+    try {
+      const data = await StudentService.cancelCourse(req);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Course Enrollment Successful'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
