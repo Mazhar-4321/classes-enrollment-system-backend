b275a2302dac8cb84db1bab8dea47a6faf15f120
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-18 10:10:14 +0530

    [Mazhar]ADDED: User Validator And Routes To The Project

--- project/src/routes/user.route.js
+++ project/src/routes/user.route.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
+import { loginURIValidator, newUserValidator } from '../validators/user.validator';
 import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
-//route to get all users
-router.get('', userController.getAllUsers);
 
 //route to create a new user
 router.post('', newUserValidator, userController.newUser);
 
 //route to get a single user by their user id
-router.get('/:id', userAuth, userController.getUser);
+router.post('/login',loginURIValidator,  userController.getUser);
 
-//route to update a single user by their user id
-router.put('/:id', userController.updateUser);
+router.post('/validateEmail',userController.validateEmail);
 
-//route to delete a single user by their user id
-router.delete('/:id', userController.deleteUser);
+router.post('/forgetPassword',userController.forgetPassword);
+
+router.post('/reset',userController.resetPassword)
+
+
 
 export default router;
